8a3b65c21fcf4d3dd22277d033a960471e4caff1
Karan Bhalodiya <karan.bhalodiya@einfochips.com>
2018-04-02 17:15:23 +0530

    ADDED: webapi for creating Delivery Job
    initial commit for creating Job

--- WebApis.py
+++ WebApis.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
 from flask import request
+import uuid
 
-from Job import *
+from Job import Job
+from settings import *
 
 
 app = Flask(__name__)
@@ -13,7 +15,7 @@ REST APIs
 
 '''
 Image uploading REST POST API
-It accepts URL list from requests and start assigning the asynchronous jobs to Celery Workers. 
+It accepts Jobs from requests and start assigning the asynchronous jobs to Celery Workers. 
 '''
 @app.route('/v1/createJob', methods=['POST'])
 def create_job():
@@ -24,9 +26,24 @@ def create_job():
 
     pass
 
+    # create a Job
+    deliveryJob = Job()
 
+    deliveryJob.packageID = uuid.uuid4().hex[:8]
+    if request.json["lat"]:
+        deliveryJob.lattitude = request.json["lat"]
+    else:
+        return jsonify({"error":"param 'Latitude' not found in json or json has not been prepared properly"}), 501
+    if request.json["lon"]:
+        deliveryJob.longitude = request.json["lon"]
+    else:
+        return jsonify({"error":"param 'Longitude' not found in json or json has not been prepared properly"}), 501
+    if request.json["valocity"]:
+        deliveryJob.valocity = request.json["valocity"]
 
-
+    deliveryJob.base_lattitude = BASELAT
+    deliveryJob.base_longitude = BASELON
+    
 
 
 
